7363fd83114baa4eef24f0b40ba8404f91537080
tutintatea <44783169+tutintatea@users.noreply.github.com>
2023-10-05 18:20:38 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+let $body = $('body');
 
+$('html').on('keydown', function(e) {
+  $bubble = $('<div>');
+
+  if (e.key == ' ') {
+    $bubble.addClass('bubble');
+    $bubble.css({
+      'top': Math.random() + (document.documentElement.clientHeight - 100),
+      'left': Math.random() + (document.documentElement.clientWidth - 100)
+    });
+    $body.append($bubble);
+  }
+});
+
+$body.on('click', '.bubble', function() {
+  $(this).addClass('is-popping');
+});
+
+$body.on('transitionend', '.bubble', function() {
+  $(this).remove();
+});
